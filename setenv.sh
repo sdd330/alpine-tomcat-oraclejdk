c16a68475efc2aabe23d802e32f9721314aac860
Leijun Yang <yang.leijun@gmail.com>
2015-06-07 16:49:55 +0300

    Add script

---
+++ setenv.sh
@@ -0,0 +1,7 @@
+# Tomcat Custom configuration
+
+CATALINA_OPTS="-server -d64 -XX:+AggressiveOpts -Djava.library.path=/usr/tomcat/lib:/usr/local/lib -Djava.awt.headless=true -XX:MaxGCPauseMillis=500 -Xmx${MEM} -Xms${MMEM} -Xincgc"
+
+if $JMX ; then
+    CATALINA_OPTS="${CATALINA_OPTS} -Dcom.sun.management.jmxremote.authenticate=false -Xdebug -Xrunjdwp:transport=dt_socket,address=62911,server=y,suspend=n -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.port=${JMX_PORT} -Djava.rmi.server.hostname=${JMX_HOSTNAME} -Dcom.sun.management.jmxremote.rmi.port=${JMX_PORT}"
+fi
