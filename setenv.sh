201bfb2885051d5180a74a62c0e3db7843b234b1
Yang Leijun <yang.leijun@gmail.com>
2015-06-07 17:00:04 +0300

    Update setenv.sh

--- setenv.sh
+++ setenv.sh
@@ -3,5 +3,5 @@
 CATALINA_OPTS="-server -d64 -XX:+AggressiveOpts -Djava.library.path=/usr/tomcat/lib:/usr/local/lib -Djava.awt.headless=true -XX:MaxGCPauseMillis=500 -Xmx${MEM} -Xms${MMEM} -Xincgc"
 
 if $JMX ; then
-    CATALINA_OPTS="${CATALINA_OPTS} -Xdebug -Xrunjdwp:transport=dt_socket,address=62911,server=y,suspend=n -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.ssl=false  -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.local.only=false -Dcom.sun.management.jmxremote.port=${JMX_PORT} -Djava.rmi.server.hostname=${JMX_HOSTNAME} -Dcom.sun.management.jmxremote.rmi.port=${JMX_PORT}"
+    CATALINA_OPTS="${CATALINA_OPTS} -Xdebug -Xrunjdwp:transport=dt_socket,address=${DEBUG_PORT},server=y,suspend=n -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.ssl=false  -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.local.only=false -Dcom.sun.management.jmxremote.port=${JMX_PORT} -Djava.rmi.server.hostname=${JMX_HOSTNAME} -Dcom.sun.management.jmxremote.rmi.port=${JMX_PORT}"
 fi
